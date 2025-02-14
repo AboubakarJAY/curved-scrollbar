70cf00199752831d39b18cd74eefeb9a4e3d898c
AboubakarJay <aboubakarjay2@hotmail.com>
2025-02-14 16:37:48 +0100

    Adding canvas

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@
 let page = document.querySelector("body");
 let containerEl = document.querySelector(".container-el");
 let scrollBar = document.querySelector(".scroll-bar");
+let scrollPiste = document.querySelector(".scroll-piste");
 for (let i = 0; i < Math.floor(window.innerHeight / 64); i++) {
     let gridContainer = document.createElement("div");
     gridContainer.className =
@@ -13,13 +14,28 @@ for (let i = 0; i < Math.floor(window.innerHeight / 64); i++) {
         gridContainer.appendChild(gridItem);
     }
 }
-function setTransform(xPos, yPos, el) {
-    el.style.transform = `translate3d(0, ${yPos}px, 0)`;
-}
-let yScrollPosition;
+let positionLeft = containerEl.getBoundingClientRect().left;
+scrollPiste.style.left = `${positionLeft + 365}px`;
 function scrollLoop() {
-    yScrollPosition = containerEl.scrollTop;
-    setTransform(0, yScrollPosition, scrollBar);
+    let position = containerEl.scrollTop;
+    scrollBar.style.top = `${position / 6}px`;
     requestAnimationFrame(scrollLoop);
+    console.log(containerEl.scrollTop);
 }
 containerEl.addEventListener("scroll", scrollLoop);
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+const x = 200;
+const y = 50;
+const width = 200;
+const height = 200;
+const radius = 30;
+ctx.beginPath();
+ctx.moveTo(x, y);
+ctx.lineTo(x + width - radius, y);
+ctx.arcTo(x + width, y, x + width, y + height, radius);
+ctx.lineTo(x + width, y + height - radius);
+ctx.strokeStyle = "orange";
+ctx.lineCap = "round";
+ctx.lineWidth = 8;
+ctx.stroke();
--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,7 @@
 let page = document.querySelector("body") as HTMLElement;
 let containerEl = document.querySelector(".container-el") as HTMLElement;
 let scrollBar = document.querySelector(".scroll-bar") as HTMLElement;
+let scrollPiste = document.querySelector(".scroll-piste") as HTMLElement;
 
 for (let i = 0; i < Math.floor(window.innerHeight / 64); i++) {
   let gridContainer = document.createElement("div");
@@ -14,16 +15,35 @@ for (let i = 0; i < Math.floor(window.innerHeight / 64); i++) {
   }
 }
 
-function setTransform(xPos: number, yPos: number, el: HTMLElement): void {
-  el.style.transform = `translate3d(0, ${yPos}px, 0)`;
-}
+let positionLeft: number = containerEl.getBoundingClientRect().left;
 
-let yScrollPosition: number;
+scrollPiste.style.left = `${positionLeft + 365}px`;
 
 function scrollLoop() {
-  yScrollPosition = containerEl.scrollTop;
-  setTransform(0, yScrollPosition, scrollBar);
+  let position: number = containerEl.scrollTop;
+  scrollBar.style.top = `${position / 6}px`;
 
   requestAnimationFrame(scrollLoop);
+  console.log(containerEl.scrollTop);
 }
 containerEl.addEventListener("scroll", scrollLoop);
+
+const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+const x = 200;
+const y = 50;
+const width = 200;
+const height = 200;
+const radius = 30;
+
+ctx.beginPath();
+ctx.moveTo(x, y);
+ctx.lineTo(x + width - radius, y);
+ctx.arcTo(x + width, y, x + width, y + height, radius);
+ctx.lineTo(x + width, y + height - radius);
+
+ctx.strokeStyle = "orange";
+ctx.lineCap = "round";
+ctx.lineWidth = 8;
+ctx.stroke();
